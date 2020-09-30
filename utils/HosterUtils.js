e4bcc7bfbe74e7d53c8434e98e305886d8c15c82
unknown <behrooz.arabzade@gmail.com>
2020-09-30 17:42:48 +0330

    publish

--- utils/HosterUtils.js
+++ utils/HosterUtils.js
@@ -238,7 +238,7 @@ HosterUtils.isFileChange = async (path, newData) => {
     try{
         console.log("isFileChange 1")
         let oldData = await fsPromises.readFile(path, 'utf8');
-        console.log("isFileChange 2")
+        console.log("isFileChange 2", oldData === newData)
         return oldData === newData;
     } catch (error) {
         console.log("isFileChange 3 error", error)
